b0f9309cd1a85d3e08ec274a734fabaaf755b6ca
Diego <diego.nicolas.gandara@gmail.com>
2021-09-11 23:26:26 -0300

    Import new Counter component and comment logo out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+import Counter from './components/Counter/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
+      <Counter />
     </div>
   );
 }
